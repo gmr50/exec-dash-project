1c1649844b6a0e7963821b12f271dce4b74e5aed
Graham Ritter <gmr50@georgetown.edu>
2019-05-05 12:55:39 -0400

    testing for to_usd

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # exec-dash-project
 
-# conda activate execDash
+
 
 Files used with this program should be kept in a folder called "data" that is placed in the same directory as the python file. 
 Plotly and the pandas packages should be installed prior to running this python file in order for it to execute. 
+Please install with: pip install -r requirements.txt
+Test using pytest
---
+++ test_exec_dash.py
@@ -0,0 +1,37 @@
+from exec_dash_revisited import to_usd
+
+
+def test_to_usd():
+
+	test_passed = False
+	input = 10000
+	result = to_usd(input)
+
+
+
+	if(result[0] == '$' and result[3] == ',' and result[-1] == '0' and result[-2] == '0'):
+		test_passed = True
+
+
+	assert test_passed == True
+
+	#reset for next test
+	#testing now with trailing decimals
+	test_passed = False
+
+	result = to_usd(10.99999999)
+
+	if(result == "$11.00"):
+		test_passed = True
+
+	assert test_passed == True
+
+	#reset for next test
+	#testing with fewer decimals
+
+	test_passed = False
+	result = to_usd(1.1)
+	if(result == "$1.10"):
+		test_passed = True
+
+	assert test_passed == True
